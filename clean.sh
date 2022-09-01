4716add2ac145865a7064a3438e15edbbc8ed2df
srgi79 <36154007+srgi79@users.noreply.github.com>
2022-09-01 18:08:46 +0200

    Add files via upload

--- clean.sh
+++ clean.sh
@@ -2,6 +2,7 @@
 
 echo "Addon Tinwoo (clean.sh): clean webpages ..."
 rm /var/www/tinwoo/index.php
+rm /var/www/tinwoo/Tinwoo.png
 
 echo "Addon Tinwoo (clean.sh): clean games symlink ..."
 rm /var/www/tinwoo/GamesSwitch
--- init.sh
+++ init.sh
@@ -7,6 +7,7 @@ mkdir /var/www/tinwoo/
 
 echo "Addon Tinwoo (init.sh): copying web files"
 cp $path/index.php /var/www/tinwoo/index.php
+cp $path/Tinwoo.png /var/www/tinwoo/Tinwoo.png
 
 echo "Addon Tinwoo (init.sh): linking games"
 ln -s /mnt/HD/HD_a2/Public/GamesSwitch /var/www/tinwoo/
